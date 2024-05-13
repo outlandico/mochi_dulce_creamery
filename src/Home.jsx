0b350a6cedb0959b630479c4229a31a67d6775a0
Adam Jimenez <outlandico@gmail.com>
2024-05-13 09:16:00 -0700

    commit

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
-import logoImage from './images/mochi_dulce_creamery_logo.png'; // Import the image file
-import './css/home.css'; // Import the CSS file
+import { Link } from 'react-router-dom';
+import logoImage from './images/mochi_dulce_creamery_logo.png';
+import frescaImage from './images/fresca.png';
+import pinaImage from './images/pina.png';
+import './css/home.css';
 
 function Home() {
   useEffect(() => {
@@ -11,16 +13,15 @@ function Home() {
       var scrollPosition = window.scrollY;
 
       if (scrollPosition > logoContainer.offsetHeight) {
-        container.style.opacity = '0'; // Hide the text when scroll position is below the logo container
+        container.style.opacity = '0';
       } else {
-        container.style.opacity = '1'; // Show the text when scroll position is above the logo container
+        container.style.opacity = '1';
       }
     });
-  }, []); // Empty dependency array ensures the effect is only applied once
+  }, []);
 
   return (
     <div>
-      {/* Menu links */}
       <div className="menu-links">
         <Link to="/">Home</Link>
         <Link to="/about">About</Link>
@@ -28,14 +29,16 @@ function Home() {
         <Link to="/contact">Contact</Link>
         <Link to="/loyalty">Loyalty</Link>
       </div>
-      {/* Logo placeholder */}
       <div className="logo-container">
         <img src={logoImage} alt="Mochi & Dulce Creamery Logo" className="logo-size" />
       </div>
-      {/* Rest of the Home component content */}
       <div className="container">
         <h1>Welcome Back!</h1>
-         </div>
+        <div className="image-container">
+          <img src={frescaImage} alt="Fresca Image" className="left-image img-size" />
+          <img src={pinaImage} alt="Pina Image" className="right-image img-size" />
+        </div>
+      </div>
     </div>
   );
 }
